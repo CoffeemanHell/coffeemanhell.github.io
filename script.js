a3d2df1ac1904d1611c5c30b52d846cefc9d7d80
Coffee Man <126782935+CoffeemanHell@users.noreply.github.com>
2024-06-24 07:06:36 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+// script.js
+document.addEventListener('DOMContentLoaded', function() {
+    const body = document.body;
+    const changeBackgroundButton = document.getElementById('changeBackgroundButton');
+    const music = document.getElementById('backgroundMusic');
+    const video = document.getElementById('backgroundVideo');
+
+    // Sayfa yüklendiğinde varsayılan arka planı ayarla
+    body.classList.add('background-1');
+
+    // Müziği döngüye al
+    music.loop = true;
+
+    function toggleBackground() {
+        if (body.classList.contains('background-1')) {
+            body.classList.remove('background-1');
+            video.style.display = 'block';
+            video.currentTime = 0; // Videoyu başa sar
+            video.play();
+        } else {
+            body.classList.add('background-1');
+            video.style.display = 'none';
+            video.pause();
+        }
+    }
+
+    changeBackgroundButton.addEventListener('click', function() {
+        toggleBackground();
+
+        // Müzik çalma/durdurma
+        if (music.paused) {
+            music.play();
+        } else {
+            music.pause();
+            music.currentTime = 0; // Müziği başa sar
+        }
+    });
+});
