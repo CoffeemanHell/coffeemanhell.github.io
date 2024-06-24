e9d222fe48f1ab346e6f68e4a65f00a1df9d243e
Coffee Man <rxbunyo@gmail.com>
2024-06-25 00:14:28 +0300

    coffeeman.hell

--- script.js
+++ script.js
@@ -1,37 +1,24 @@
 document.addEventListener('DOMContentLoaded', function() {
     const body = document.body;
-    const changeBackgroundButton = document.getElementById('changeBackgroundButton');
+    const toggleButton = document.getElementById('toggleButton');
     const music = document.getElementById('backgroundMusic');
     const video = document.getElementById('backgroundVideo');
 
-    // Sayfa yüklendiğinde varsayılan arka planı ayarla
-    body.classList.add('background-1');
-
-    // Müziği döngüye al
-    music.loop = true;
-
-    function toggleBackground() {
-        if (body.classList.contains('background-1')) {
-            body.classList.remove('background-1');
+    function toggleBackgroundAndMusic() {
+        body.classList.toggle('video-background');
+        
+        if (body.classList.contains('video-background')) {
             video.style.display = 'block';
-            video.currentTime = 0; // Videoyu başa sar
+            video.currentTime = 0;
             video.play();
+            music.play();
         } else {
-            body.classList.add('background-1');
             video.style.display = 'none';
             video.pause();
+            music.pause();
+            music.currentTime = 0;
         }
     }
 
-    changeBackgroundButton.addEventListener('click', function() {
-        toggleBackground();
-
-        // Müzik çalma/durdurma
-        if (music.paused) {
-            music.play();
-        } else {
-            music.pause();
-            music.currentTime = 0; // Müziği başa sar
-        }
-    });
+    toggleButton.addEventListener('click', toggleBackgroundAndMusic);
 });
