dbc3772547da01826097b114e541185f4bcaf5bb
Coffee Man <rxbunyo@gmail.com>
2024-06-24 07:42:33 +0300

    coffeeman.hell

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-// script.js
 document.addEventListener('DOMContentLoaded', function() {
     const body = document.body;
     const changeBackgroundButton = document.getElementById('changeBackgroundButton');
